ec1646232c38143a2891c676607e64e2b00f567f
mjshuff23 <mjshuff23@gmail.com>
2021-06-13 20:52:12 -0400

    removed solution

--- sample-app/src/App.js
+++ sample-app/src/App.js
@@ -1,35 +1,33 @@
 import React from 'react';
 
 function App() {
-  return (
-    <>
-      <header>
-        <h1 id="site-header">Welcome To My Super Responsive Site!</h1>
-      </header>
-      <main>
-        <section id="section-one">
-          <h2 id="main-header">Section One Title</h2>
-          <div id="main-logo"></div>
-          <p>
-            I'm baby slow-carb put a bird on it intelligentsia pug occupy irony
-            deep v disrupt taiyaki. Kombucha selvage tumblr pug, XOXO subway tile
-            chia waistcoat art party fam shabby chic swag. Cronut succulents tofu
-            retro aesthetic. Gochujang flannel fingerstache, hella godard venmo
-            umami taiyaki semiotics fanny pack vegan. Viral williamsburg lo-fi
-            microdosing, chillwave hella vape quinoa trust fund put a bird on it
-            polaroid tbh fixie hexagon fingerstache. Bespoke franzen tattooed
-            everyday carry post-ironic authentic vexillologist.
-          </p>
-        </section>
-        <section id="section-two">
-          <h2 id="section-two-header">Section Two Title</h2>
-          <p>
-            Umami taxidermy quis neutra. Brooklyn street art fam veniam PBR&B vinyl id irure flannel anim fugiat chillwave YOLO gentrify. Tousled williamsburg before they sold out actually messenger bag, leggings banh mi pabst helvetica dreamcatcher. Yuccie helvetica ethical, umami pok pok quinoa snackwave commodo man bun sustainable. Four dollar toast chia VHS salvia selfies. Chia fugiat aute mustache thundercats, 3 wolf moon flannel jean shorts.
-          </p>
-        </section>
-      </main>
-    </>
-  );
+	return (
+		<>
+			<h1 id="site-header">Welcome To My Super Responsive Site!</h1>
+			<h2 id="main-header">Section One Title</h2>
+			<div id="main-logo"></div>
+			<p>
+				I'm baby slow-carb put a bird on it intelligentsia pug occupy irony deep
+				v disrupt taiyaki. Kombucha selvage tumblr pug, XOXO subway tile chia
+				waistcoat art party fam shabby chic swag. Cronut succulents tofu retro
+				aesthetic. Gochujang flannel fingerstache, hella godard venmo umami
+				taiyaki semiotics fanny pack vegan. Viral williamsburg lo-fi
+				microdosing, chillwave hella vape quinoa trust fund put a bird on it
+				polaroid tbh fixie hexagon fingerstache. Bespoke franzen tattooed
+				everyday carry post-ironic authentic vexillologist.
+			</p>
+			<h2 id="section-two-header">Section Two Title</h2>
+			<p>
+				Umami taxidermy quis neutra. Brooklyn street art fam veniam PBR&B vinyl
+				id irure flannel anim fugiat chillwave YOLO gentrify. Tousled
+				williamsburg before they sold out actually messenger bag, leggings banh
+				mi pabst helvetica dreamcatcher. Yuccie helvetica ethical, umami pok pok
+				quinoa snackwave commodo man bun sustainable. Four dollar toast chia VHS
+				salvia selfies. Chia fugiat aute mustache thundercats, 3 wolf moon
+				flannel jean shorts.
+			</p>
+		</>
+	);
 }
 
 export default App;
