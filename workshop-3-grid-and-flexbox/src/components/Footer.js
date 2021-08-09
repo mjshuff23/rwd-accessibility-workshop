39d39fa33237bbc064bc9e411e7ad0a44b97ee43
mjshuff23 <mjshuff23@gmail.com>
2021-08-09 03:35:05 -0400

    update

--- workshop-3-grid-and-flexbox/src/components/Footer.js
+++ workshop-3-grid-and-flexbox/src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './styles/Footer.css';
 
 function Footer() {
 	return <div className="Footer">Terms and Conditions | Privacy Policy</div>;
--- workshop-3-grid-and-flexbox/src/components/Main.js
+++ workshop-3-grid-and-flexbox/src/components/Main.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Image from './Image';
+import './styles/Main.css';
 
 function Main() {
 	const images = [
