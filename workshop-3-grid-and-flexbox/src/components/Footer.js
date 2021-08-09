62a718845f9705ea228d152cf6579eec15433cf4
mjshuff23 <mjshuff23@gmail.com>
2021-08-09 03:42:13 -0400

    removed solution

--- workshop-3-grid-and-flexbox/src/components/Footer.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './styles/Footer.css';
-
-function Footer() {
-	return <div className="Footer">Terms and Conditions | Privacy Policy</div>;
-}
-
-export default Footer;
